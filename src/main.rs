c173666de8663765e6d88ec67d7c5cdbfd8a9cce
6z7y <4bum0u54.tp#gmail.com>
2025-05-22 01:02:08 +0300

    v0.0.4

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::{
-    fs::{OpenOptions, File},
-    io::{BufRead, BufReader, Result, Write}, 
+    fs::{File, OpenOptions},
+    io::{BufRead, BufReader, Result, Write} 
+};
+
+mod structfn;
+use crate::structfn::{
+    EchoSystem,
+    Opration,
+    Time
 };
 
 const FILE_PATH: &str = "/tmp/echotale_history.txt";
-
-mod structfn;
-use crate::structfn::EchoSystem;
 
 fn main() -> Result<()> {
     let mut logger = OpenOptions::new()
@@ -19,12 +23,52 @@ fn main() -> Result<()> {
     println!("Wait a moment");
     EchoSystem::sleeep(2500);
 
-
     loop {
         let user_output = EchoSystem::print_new("type (':?' for help): ")?;
+        if user_output.is_empty() {
+            println!("empty text!");
+            continue;
+        } else {
+            println!("You weote: {}", user_output);
+            writeln!(logger, "{}", user_output)?;
 
-        if !user_output.is_empty() {
-            if user_output == ":clear" {
+            if user_output == ":calculate" {
+                println!("\nHint: '2 * 5'\n");
+                let prompt = EchoSystem::print_new("cacl> ")?;
+
+                let parts: Vec<&str> = prompt.split_whitespace().collect();
+                if parts.len() != 3 {
+                    println!("type like (3 + 4)");
+                    continue;
+                }
+
+                let a = match parts[0].parse::<f64>() {
+                    Ok(n) => n,
+                    Err(_) => {
+                        println!("first number invalid");
+                        continue;
+                    }
+                };
+
+                let op = match Opration::from_str(parts[1]) {
+                    Some(op) => op,
+                    None => {
+                        println!("error type (+ - * /)");
+                        continue;
+                    }
+                };
+
+                let b = match parts[2].parse::<f64>() {
+                    Ok(n) => n,
+                    Err(_) => {
+                        println!("second number invalid");
+                        continue;
+                    }
+                };
+
+                let result = op.apply(a, b);
+                println!("= {}", result)
+            } else if user_output == ":clear" {
                 std::fs::write(FILE_PATH, "")?;
                 println!("History cleared.");
             } else if user_output == ":show" {
@@ -62,23 +106,39 @@ fn main() -> Result<()> {
                         println!("- {}", line);
                     }
                 } 
+            } else if user_output == ":time" {
+                println!("\nHint: '23:59'\n");
+                let prompt_input = match EchoSystem::print_new("time> ") {
+                    Ok(input) => input,
+                    Err(e) => {
+                        println!("Input error: {}", e);
+                        String::new()
+                    }
+                };
+
+                match Time::from_str(&prompt_input) {
+                    Ok(time) => {
+                        time.show();
+                        println!("Total minute: {}", time.to_minute());
+                        println!("Total secound: {}", time.to_second());
+                    }
+                    Err(msg) => println!("invlid format: {}", msg)
+                }
+                
             } else if user_output == ":quit" {
                 break;
             } else if user_output == ":?" {
                 println!("
                 \nAvailable commands:\n\
+                    :calculate  - Calculate menu\n\
                     :clear      - Clear history\n\
                     :show       - Show history\n\
                     :search     - Search history\n\
+                    :time       - Time System\n\
                     :quit       - Exit the program\n\
                     :?          - Show this help message\n\
                     ")
-            } else {
-                println!("You weote: {}", user_output);
-                writeln!(logger, "{}", user_output)?;
             }
-        } else {
-            println!("empty text!");
         }
     }
     print!("file path: /tmp/echotale_history.txt");
--- src/structfn.rs
+++ src/structfn.rs
@@ -1,8 +1,18 @@
-use std::{
-    io::{self, Result, Write}
-};
+use std::io::{self, Write};
 
 pub struct EchoSystem;
+
+pub enum Opration {
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+}
+
+pub struct Time {
+    pub hour: u32,
+    pub minute: u32,
+}
 
 impl EchoSystem {
     pub fn draw_ascii() {
@@ -14,7 +24,7 @@ impl EchoSystem {
         "#);
     }
 
-    pub fn print_new(prompt: &str) -> Result<String> {
+    pub fn print_new(prompt: &str) -> io::Result<String> {
         print!("{}", prompt);
         io::stdout().flush()?;
 
@@ -26,5 +36,61 @@ impl EchoSystem {
 
     pub fn sleeep(time: u64) {
         std::thread::sleep(std::time::Duration::from_millis(time))
+    }
+}
+
+impl Time {
+    pub fn from_str(text: &str) -> Result<Self, String> {
+        let parts: Vec<&str> = text.split(':').collect();
+        if parts.len() != 2 {
+            return Err("Error0".to_string());
+        }
+        
+        let hour = parts[0].parse::<u32>().map_err(|_| "inviled hour".to_string())?;
+        let minute = parts[1].parse::<u32>().map_err(|_| "inviled minute".to_string())?;
+
+        if hour > 23 || minute > 59 {
+            return Err("Hour must be 0-23, and minute 0-59".to_string());
+        }
+
+        Ok(Self { hour, minute })
+    }
+
+    pub fn to_minute(&self) -> u32 {
+        self.hour * 60 + self.minute
+    }
+
+    pub fn to_second(&self) -> u32 {
+        (self.hour * 60 + self.minute) * 60
+    }
+
+    pub fn show(&self) {
+        println!("{:02}:{:02}", self.hour, self.minute);
+    }
+}
+
+impl Opration {
+    pub fn from_str(op: &str) -> Option<Self> {
+        match op {
+            "+" => Some(Opration::Add),
+            "-" => Some(Opration::Subtract),
+            "*" => Some(Opration::Multiply),
+            "/" => Some(Opration::Divide),
+            _ => None
+        }
+    }
+
+    pub fn apply(&self, a: f64, b: f64) -> f64 {
+        match self {
+            Self::Add => a + b,
+            Self::Subtract => a - b,
+            Self::Multiply => a * b,
+            Self::Divide => {
+                if b == 0.0 {
+                    println!("Cannot Divide by zero!")
+                }
+                a / b
+            }
+        }
     }
 }
